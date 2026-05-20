dec32c5f75a373a018fa15e52fdc81b17e1b74fe
chap989 <rcha0087@student.monash.edu>
2026-05-20 11:07:51 +1000

    gets infor token and authorising it

--- Infor_API_connector.py
+++ Infor_API_connector.py
@@ -1,5 +1,32 @@
-import fastapi from FASTAPI, request
-import httpx
+from fastapi import FastAPI, Request, APIRouter
+import connector_config
+
+router = APIRouter(prefix="/infor", tags=["INFOR M3"])
+
+
+# function to get the api token
+
+async def get_infor_token(client):
+    payload = {
+        "grant_type": "password",
+        "client_id": connector_config.INFOR_CLIENT_ID,
+        "client_secret": connector_config.INFOR_CLIENT_SECRET,
+        "username": connector_config.INFOR_USERNAME,
+        "password": connector_config.INFOR_PASSWORD,
+    }
+
+    # sends login details to Infor and waits and gets response
+    response = await client.post(
+        connector_config.INFOR_TOKEN_URL,
+        data=payload,
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+
+    # checks if the http request has errors
+    response.raise_for_status()
+    return response.json()["access_token"] # Infor returns Json
+
+
 
 
 
